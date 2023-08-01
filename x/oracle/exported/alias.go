a528a15ac46797bfab50eeab839e6cfedcb1267f
Christian Lohr <christian@volume.finance>
2023-08-01 18:12:40 +0200

    WIP start pulling in terra oracle

---
+++ x/oracle/exported/alias.go
@@ -0,0 +1,9 @@
+// DONTCOVER
+package exported
+
+import "github.com/palomachain/paloma/x/oracle/types"
+
+type (
+	MsgAggregateExchangeRatePrevote = types.MsgAggregateExchangeRatePrevote
+	MsgAggregateExchangeRateVote    = types.MsgAggregateExchangeRateVote
+)
